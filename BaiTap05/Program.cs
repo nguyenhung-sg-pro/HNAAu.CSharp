445c9d6a3bcdc56f93cf0692d5fab2a16e57774d
nguyenhung-sg-pro <nguyenhung.sg.pro@gmail.com>
2026-06-18 08:45:32 +0700

    Add BaiTap05

---
+++ BaiTap05/Program.cs
@@ -0,0 +1,35 @@
+﻿class DongVatCoVu
+{
+    public string Loai = "Động vật có vú";
+    public int SoChan = 4;
+    public void SinhSan()
+    {
+        Console.WriteLine("Nuôi con bằng sữa mẹ!");
+    }
+}
+
+class Meo : DongVatCoVu
+{
+    public string Ten = "";
+    public void BatChuot()
+    {
+        Console.WriteLine("Mèo đang bắt chuột");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello bạn đến với chương trình của tôi!");
+        Meo meo = new();
+
+        meo.Ten = "Mèo mướp";
+        Console.WriteLine(meo.Ten.ToString());
+        meo.BatChuot();
+
+        Console.WriteLine(meo.Loai.ToString());
+        meo.SinhSan();
+
+    }
+}
