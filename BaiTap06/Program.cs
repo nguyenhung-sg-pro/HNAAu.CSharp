40ab35ba094bc0e077aa27bd1a82472b5444a330
nguyenhung-sg-pro <nguyenhung.sg.pro@gmail.com>
2026-06-20 08:37:02 +0700

    Add BaiTap06 - Tinh Da Hinh

---
+++ BaiTap06/Program.cs
@@ -0,0 +1,33 @@
+﻿public abstract class DongVat
+{
+    public string? Ten { get; set; }
+
+    public abstract void Speak();
+}
+
+public class Cho : DongVat
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Gâu Gâu !");
+    }
+}
+
+public class Meo : DongVat
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Meo meo !");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        DongVat dv1 = new Meo();
+        dv1.Speak();
+
+        DongVat dv2 = new Cho();
+        dv2.Speak();
+    }
+}
