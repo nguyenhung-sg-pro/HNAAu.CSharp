f7372011025439075484e47e1f334d40e14d08af
nguyenhung-sg-pro <nguyenhung.sg.pro@gmail.com>
2026-07-07 09:01:24 +0700

    add new project BaiTap10

---
+++ BaiTap10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void ThuatToan()
+    {
+        for (double i = 1; i < 10000000000; i++) { }
+    }
+    public static void Main()
+    {
+        /*
+        int n = 10;
+
+        //Thuật toán O(n)
+        Console.WriteLine("Thuật toán O(n)");
+        for (int i = 0; i < n; i++)
+            Console.Write(i + " ");
+        Console.WriteLine();
+
+        // Thuật toán O(n^2)
+        Console.WriteLine("Thuật toán O(n^2)");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                Console.Write("* ");
+            Console.WriteLine();
+        }*/
+
+        // //1. Đo bằng dateTime
+        // var start = DateTime.Now;
+        // ThuatToan();
+        // var duration = DateTime.Now - start;
+        // Console.WriteLine("Thời gian thực thi: " + duration.TotalMilliseconds);
+
+        //2. Stopwatch
+        var stopWatch = Stopwatch.StartNew();
+        ThuatToan();
+        stopWatch.Stop();
+        Console.WriteLine("Thời gian thực thi tính bằng stopWatch : " + stopWatch.ElapsedMilliseconds + " ms");
+    }
+}
