c90c142fe4726f63cf05e0cd9f854fc0f9979efa
nguyenhung-sg-pro <nguyenhung.sg.pro@gmail.com>
2026-07-09 08:57:57 +0700

    add code BaiTap11

---
+++ BaiTap11/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static int GiaiThua(int n)
+    {
+        if (n == 0) return 1;
+        return n * GiaiThua(n - 1);
+    }
+    public static void Main()
+    {
+        int n = 4;
+        int kq = GiaiThua(n);
+        Console.WriteLine($"Giai thừa của {n} = {kq}");
+    }
+}
